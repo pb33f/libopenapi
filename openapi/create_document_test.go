3d0901524f362df6c910c7c54bc780edcf44f3e0
Dave Shanley <dave@quobix.com>
2022-07-30 22:02:05 -0400

    Adding tests to cover model as we grow it.

--- openapi/create_document_test.go
+++ openapi/create_document_test.go
@@ -19,4 +19,14 @@ func TestCreateDocument(t *testing.T) {
     doc, err := CreateDocument(data)
     assert.NotNil(t, doc)
     assert.NoError(t, err)
+
+    assert.Equal(t, "3.0.1", doc.Version.Value)
+    assert.Equal(t, "Burger Shop", doc.Info.Value.Title.Value)
+    assert.NotEmpty(t, doc.Info.Value.Title.Value)
+    assert.Equal(t, "https://pb33f.io", doc.Info.Value.TermsOfService.Value)
+    assert.Equal(t, "pb33f", doc.Info.Value.Contact.Value.Name.Value)
+    assert.Equal(t, "buckaroo@pb33f.io", doc.Info.Value.Contact.Value.Email.Value)
+    assert.Equal(t, "https://pb33f.io", doc.Info.Value.Contact.Value.URL.Value)
+    assert.Equal(t, "1.2", doc.Info.Value.Version.Value)
+
 }
