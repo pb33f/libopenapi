7acd09632112d03adff30372c5135d29f9f10351
Dave Shanley <dave@quobix.com>
2022-08-15 21:36:12 -0400

    fixed typo added
    computer went strange, random chars picked up from keyboard.
    
    or I was just a bit drunk.

--- datamodel/low/3.0/path_test.go
+++ datamodel/low/3.0/path_test.go
@@ -223,7 +223,7 @@ func TestPaths_Build_BadRef(t *testing.T) {
 
 func TestPathItem_Build_GoodRef(t *testing.T) {
 
-	// this is kinda nuts, it's also not illegal, however the mechanics still need to work.bfes
+	// this is kinda nuts, it's also not illegal, however the mechanics still need to work.
 	yml := `"/some/path":
  description: this is some path
  get:
