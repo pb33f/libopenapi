6c2de6c151db6b1da14f89aa4ca4788f0b4f9fdf
Dave Shanley <dave@quobix.com>
2022-08-22 09:24:23 -0400

    The resolver just got an upgrade, polymorphic circular reference checking.
    Building out models that have circular references is tricky. Particularly when they are polymorphic ones. The resolver now searches the polymorphic references for circular references as well (which is returning much more valuable results now). The low level model is now sensitive to this and will avoid building out anything that is circular.

---
+++ index/circular_reference_result.go
@@ -0,0 +1,22 @@
+package index
+
+import "strings"
+
+// CircularReferenceResult contains a circular reference found when traversing the graph.
+type CircularReferenceResult struct {
+	Journey   []*Reference
+	Start     *Reference
+	LoopIndex int
+	LoopPoint *Reference
+}
+
+func (c *CircularReferenceResult) GenerateJourneyPath() string {
+	buf := strings.Builder{}
+	for i, ref := range c.Journey {
+		buf.WriteString(ref.Name)
+		if i+1 < len(c.Journey) {
+			buf.WriteString(" -> ")
+		}
+	}
+	return buf.String()
+}
