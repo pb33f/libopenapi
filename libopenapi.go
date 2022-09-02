01d67f1fd89fd6771eee22d30010ed3e65e336c7
Dave Shanley <dave@quobix.com>
2022-09-02 10:41:06 -0400

    Testing out mutation
    works great! just as I expected. (this will be the) best openapi library out there!

--- libopenapi.go
+++ libopenapi.go
@@ -1,7 +1,30 @@
 package main
 
-import "github.com/pb33f/libopenapi/utils"
-
 func main() {
-	utils.BuildPath("nope", []string{"one"})
+
+	//	testData := `openapi: 3.0.1
+	//info:
+	//  title: this is a title
+	//  description: this is a description`
+	//
+	//	data := []byte(testData)
+	//	_ = ioutil.WriteFile("sample.yaml", data, 0664)
+	//
+	//	info, _ := datamodel.ExtractSpecInfo(data)
+	//	lowDoc, err := low.CreateDocument(info)
+	//	if len(err) > 0 {
+	//		for e := range err {
+	//			fmt.Printf("%e\n", err[e])
+	//		}
+	//		return
+	//	}
+	//	highDoc := high.NewDocument(lowDoc)
+	//	fmt.Println(highDoc.Info.Title)
+	//	highDoc.Info.GoLow().Title.ValueNode.Value = "let's hack this shizzle"
+	//
+	//	modified, _ := yaml.Marshal(info.RootNode)
+	//	fmt.Println(string(modified))
+
+	//os.Remove("sample.yaml")
+
 }
