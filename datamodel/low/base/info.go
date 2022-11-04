5601ad17438dfdc3d3ba259bcfd8e83d759a3c0b
Daniel G. Taylor <dtaylor@istreamplanet.com>
2022-11-04 13:20:04 -0700

    feat: add extensions to info

--- datamodel/low/base/info.go
+++ datamodel/low/base/info.go
@@ -14,8 +14,8 @@ import (
 // The object provides metadata about the API. The metadata MAY be used by the clients if needed, and MAY be presented
 // in editing or documentation generation tools for convenience.
 //
-//  v2 - https://swagger.io/specification/v2/#infoObject
-//  v3 - https://spec.openapis.org/oas/v3.1.0#info-object
+//	v2 - https://swagger.io/specification/v2/#infoObject
+//	v3 - https://spec.openapis.org/oas/v3.1.0#info-object
 type Info struct {
 	Title          low.NodeReference[string]
 	Description    low.NodeReference[string]
@@ -23,10 +23,18 @@ type Info struct {
 	Contact        low.NodeReference[*Contact]
 	License        low.NodeReference[*License]
 	Version        low.NodeReference[string]
+	Extensions     map[low.KeyReference[string]]low.ValueReference[any]
+}
+
+// FindExtension attempts to locate an extension with the supplied key
+func (i *Info) FindExtension(ext string) *low.ValueReference[any] {
+	return low.FindItemInMap(ext, i.Extensions)
 }
 
 // Build will extract out the Contact and Info objects from the supplied root node.
 func (i *Info) Build(root *yaml.Node, idx *index.SpecIndex) error {
+	i.Extensions = low.ExtractExtensions(root)
+
 	// extract contact
 	contact, _ := low.ExtractObject[*Contact](ContactLabel, root, idx)
 	i.Contact = contact
