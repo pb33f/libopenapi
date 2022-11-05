e85887fe1d3459bd33e1dd7cc9c89f9882a2635a
Dave Shanley <dave@quobix.com>
2022-11-05 09:55:13 -0400

    Bumped low level v2 examples test coverage

--- datamodel/low/v2/examples_test.go
+++ datamodel/low/v2/examples_test.go
@@ -2,3 +2,49 @@
 // SPDX-License-Identifier: MIT
 
 package v2
+
+import (
+	"github.com/pb33f/libopenapi/datamodel/low"
+	"github.com/pb33f/libopenapi/index"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
+	"testing"
+)
+
+func TestExamples_Hash(t *testing.T) {
+
+	yml := `something: string
+yes:
+  - more
+  - water
+anything:
+  cake: burger
+nothing: int`
+
+	var idxNode yaml.Node
+	_ = yaml.Unmarshal([]byte(yml), &idxNode)
+	idx := index.NewSpecIndex(&idxNode)
+
+	var n Examples
+	_ = low.BuildModel(idxNode.Content[0], &n)
+	_ = n.Build(idxNode.Content[0], idx)
+
+	yml2 := `anything:
+  cake: burger
+something: string
+nothing: int
+yes:
+  - more
+  - water`
+
+	var idxNode2 yaml.Node
+	_ = yaml.Unmarshal([]byte(yml2), &idxNode2)
+	idx2 := index.NewSpecIndex(&idxNode2)
+
+	var n2 Examples
+	_ = low.BuildModel(idxNode2.Content[0], &n2)
+	_ = n2.Build(idxNode2.Content[0], idx2)
+
+	assert.Equal(t, n.Hash(), n2.Hash())
+
+}
