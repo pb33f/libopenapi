ce82d42ce361d7262ba5ceb2b9c81210873ddf51
Dave Shanley <dave@quobix.com>
2022-11-05 10:28:07 -0400

    Bumped coverage on headers and items in v2 package.
    Starting to clean up the cross interface mess that I created when building what's changed for these pain in the ass objects that vary so much between  versions. We don't need super shared interfaces with all the properties populated for this to be useful.

--- datamodel/low/model_interfaces.go
+++ datamodel/low/model_interfaces.go
@@ -45,9 +45,45 @@ type SharedParameterHeader interface {
 	GetAllowEmptyValue() *NodeReference[bool]
 }
 
-type IsHeader interface {
-	SharedParameters
-	SharedParameterHeader
+type HasDescription interface {
+	GetDescription() *NodeReference[string]
+}
+
+type SwaggerHeader interface {
+	Hash() [32]byte
+	GetType() *NodeReference[string]
+	GetDescription() *NodeReference[string]
+	GetFormat() *NodeReference[string]
+	GetCollectionFormat() *NodeReference[string]
+	GetDefault() *NodeReference[any]
+	GetMaximum() *NodeReference[int]
+	GetExclusiveMaximum() *NodeReference[bool]
+	GetMinimum() *NodeReference[int]
+	GetExclusiveMinimum() *NodeReference[bool]
+	GetMaxLength() *NodeReference[int]
+	GetMinLength() *NodeReference[int]
+	GetPattern() *NodeReference[string]
+	GetMaxItems() *NodeReference[int]
+	GetMinItems() *NodeReference[int]
+	GetUniqueItems() *NodeReference[bool]
+	GetEnum() *NodeReference[[]ValueReference[string]]
+	GetMultipleOf() *NodeReference[int]
+	GetItems() *NodeReference[any] // requires cast.
+}
+
+type OpenAPIHeader interface {
+	Hash() [32]byte
+	GetDescription() *NodeReference[string]
+	GetDeprecated() *NodeReference[bool]
+	GetStyle() *NodeReference[string]
+	GetAllowReserved() *NodeReference[bool]
+	GetExplode() *NodeReference[bool]
+	GetExample() *NodeReference[any]
+	GetRequired() *NodeReference[bool]
+	GetAllowEmptyValue() *NodeReference[bool]
+	GetSchema() *NodeReference[any]   // requires cast.
+	GetExamples() *NodeReference[any] // requires cast
+	GetContent() *NodeReference[any]  // requires cast.
 }
 
 type SharedOperations interface {
