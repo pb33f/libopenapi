d9ae94519c10cf77955b30ffc33e76c159d32ff3
Dave Shanley <dave@quobix.com>
2022-11-22 17:11:10 -0500

    Fixed some glitches with changes on examples in `what-changed`
    Running some stress tests triggered some failures, still needs coverage but is operational and accurate.

--- what-changed/model/media_type.go
+++ what-changed/model/media_type.go
@@ -4,96 +4,136 @@
 package model
 
 import (
-	"github.com/pb33f/libopenapi/datamodel/low/v3"
+    "fmt"
+    "github.com/pb33f/libopenapi/datamodel/low"
+    "github.com/pb33f/libopenapi/datamodel/low/v3"
+    "github.com/pb33f/libopenapi/utils"
+    "gopkg.in/yaml.v3"
 )
 
 // MediaTypeChanges represent changes made between two OpenAPI MediaType instances.
 type MediaTypeChanges struct {
-	PropertyChanges
-	SchemaChanges    *SchemaChanges              `json:"schemas,omitempty" yaml:"schemas,omitempty"`
-	ExtensionChanges *ExtensionChanges           `json:"extensions,omitempty" yaml:"extensions,omitempty"`
-	ExampleChanges   map[string]*ExampleChanges  `json:"examples,omitempty" yaml:"examples,omitempty"`
-	EncodingChanges  map[string]*EncodingChanges `json:"encoding,omitempty" yaml:"encoding,omitempty"`
+    PropertyChanges
+    SchemaChanges    *SchemaChanges              `json:"schemas,omitempty" yaml:"schemas,omitempty"`
+    ExtensionChanges *ExtensionChanges           `json:"extensions,omitempty" yaml:"extensions,omitempty"`
+    ExampleChanges   map[string]*ExampleChanges  `json:"examples,omitempty" yaml:"examples,omitempty"`
+    EncodingChanges  map[string]*EncodingChanges `json:"encoding,omitempty" yaml:"encoding,omitempty"`
 }
 
 // TotalChanges returns the total number of changes between two MediaType instances.
 func (m *MediaTypeChanges) TotalChanges() int {
-	c := m.PropertyChanges.TotalChanges()
-	for k := range m.ExampleChanges {
-		c += m.ExampleChanges[k].TotalChanges()
-	}
-	if m.SchemaChanges != nil {
-		c += m.SchemaChanges.TotalChanges()
-	}
-	if len(m.EncodingChanges) > 0 {
-		for i := range m.EncodingChanges {
-			c += m.EncodingChanges[i].TotalChanges()
-		}
-	}
-	if m.ExtensionChanges != nil {
-		c += m.ExtensionChanges.TotalChanges()
-	}
-	return c
+    c := m.PropertyChanges.TotalChanges()
+    for k := range m.ExampleChanges {
+        c += m.ExampleChanges[k].TotalChanges()
+    }
+    if m.SchemaChanges != nil {
+        c += m.SchemaChanges.TotalChanges()
+    }
+    if len(m.EncodingChanges) > 0 {
+        for i := range m.EncodingChanges {
+            c += m.EncodingChanges[i].TotalChanges()
+        }
+    }
+    if m.ExtensionChanges != nil {
+        c += m.ExtensionChanges.TotalChanges()
+    }
+    return c
 }
 
 // TotalBreakingChanges returns the total number of breaking changes made between two MediaType instances.
 func (m *MediaTypeChanges) TotalBreakingChanges() int {
-	c := m.PropertyChanges.TotalBreakingChanges()
-	for k := range m.ExampleChanges {
-		c += m.ExampleChanges[k].TotalBreakingChanges()
-	}
-	if m.SchemaChanges != nil {
-		c += m.SchemaChanges.TotalBreakingChanges()
-	}
-	if len(m.EncodingChanges) > 0 {
-		for i := range m.EncodingChanges {
-			c += m.EncodingChanges[i].TotalBreakingChanges()
-		}
-	}
-	return c
+    c := m.PropertyChanges.TotalBreakingChanges()
+    for k := range m.ExampleChanges {
+        c += m.ExampleChanges[k].TotalBreakingChanges()
+    }
+    if m.SchemaChanges != nil {
+        c += m.SchemaChanges.TotalBreakingChanges()
+    }
+    if len(m.EncodingChanges) > 0 {
+        for i := range m.EncodingChanges {
+            c += m.EncodingChanges[i].TotalBreakingChanges()
+        }
+    }
+    return c
 }
 
 // CompareMediaTypes compares a left and a right MediaType object for any changes. If found, a pointer to a
 // MediaTypeChanges instance is returned, otherwise nothing is returned.
 func CompareMediaTypes(l, r *v3.MediaType) *MediaTypeChanges {
 
-	var props []*PropertyCheck
-	var changes []*Change
+    var props []*PropertyCheck
+    var changes []*Change
 
-	mc := new(MediaTypeChanges)
+    mc := new(MediaTypeChanges)
 
-	// Example
-	addPropertyCheck(&props, l.Example.ValueNode, r.Example.ValueNode,
-		l.Example.Value, r.Example.Value, &changes, v3.ExampleLabel, false)
+    if low.AreEqual(l, r) {
+        return nil
+    }
 
-	CheckProperties(props)
+    // Example
+    if !l.Example.IsEmpty() && !r.Example.IsEmpty() {
+        if (utils.IsNodeMap(l.Example.ValueNode) && utils.IsNodeMap(r.Example.ValueNode)) ||
+            (utils.IsNodeArray(l.Example.ValueNode) && utils.IsNodeArray(r.Example.ValueNode)) {
+            render, err := yaml.Marshal(l.Example.ValueNode)
+            if err != nil {
+                l.Example.ValueNode.Value = fmt.Sprintf("%s", render)
+            }
+            render, err = yaml.Marshal(r.Example.ValueNode)
+            if err == nil {
+                r.Example.ValueNode.Value = fmt.Sprintf("%s", render)
+            }
+        }
+        addPropertyCheck(&props, l.Example.ValueNode, r.Example.ValueNode,
+            l.Example.Value, r.Example.Value, &changes, v3.ExampleLabel, false)
 
-	// schema
-	if !l.Schema.IsEmpty() && !r.Schema.IsEmpty() {
-		mc.SchemaChanges = CompareSchemas(l.Schema.Value, r.Schema.Value)
-	}
-	if !l.Schema.IsEmpty() && r.Schema.IsEmpty() {
-		CreateChange(&changes, ObjectRemoved, v3.SchemaLabel, l.Schema.ValueNode,
-			nil, true, l.Schema.Value, nil)
-	}
-	if l.Schema.IsEmpty() && !r.Schema.IsEmpty() {
-		CreateChange(&changes, ObjectAdded, v3.SchemaLabel, nil,
-			r.Schema.ValueNode, true, nil, r.Schema.Value)
-	}
+    } else {
 
-	// examples
-	mc.ExampleChanges = CheckMapForChanges(l.Examples.Value, r.Examples.Value,
-		&changes, v3.ExamplesLabel, CompareExamples)
+        if utils.IsNodeMap(l.Example.ValueNode) || utils.IsNodeArray(l.Example.ValueNode) {
+            render, err := yaml.Marshal(l.Example.ValueNode)
+            if err != nil {
+                l.Example.ValueNode.Value = fmt.Sprint(render)
+            }
+        }
 
-	// encoding
-	mc.EncodingChanges = CheckMapForChanges(l.Encoding.Value, r.Encoding.Value,
-		&changes, v3.EncodingLabel, CompareEncoding)
+        if utils.IsNodeMap(r.Example.ValueNode) || utils.IsNodeArray(r.Example.ValueNode) {
+            render, err := yaml.Marshal(r.Example.ValueNode)
+            if err == nil {
+                r.Example.ValueNode.Value = fmt.Sprintf("%s", render)
+            }
+        }
 
-	mc.ExtensionChanges = CompareExtensions(l.Extensions, r.Extensions)
-	mc.Changes = changes
+        addPropertyCheck(&props, l.Example.ValueNode, r.Example.ValueNode,
+            l.Example.Value, r.Example.Value, &changes, v3.ExampleLabel, false)
+    }
 
-	if mc.TotalChanges() <= 0 {
-		return nil
-	}
-	return mc
+    CheckProperties(props)
+
+    // schema
+    if !l.Schema.IsEmpty() && !r.Schema.IsEmpty() {
+        mc.SchemaChanges = CompareSchemas(l.Schema.Value, r.Schema.Value)
+    }
+    if !l.Schema.IsEmpty() && r.Schema.IsEmpty() {
+        CreateChange(&changes, ObjectRemoved, v3.SchemaLabel, l.Schema.ValueNode,
+            nil, true, l.Schema.Value, nil)
+    }
+    if l.Schema.IsEmpty() && !r.Schema.IsEmpty() {
+        CreateChange(&changes, ObjectAdded, v3.SchemaLabel, nil,
+            r.Schema.ValueNode, true, nil, r.Schema.Value)
+    }
+
+    // examples
+    mc.ExampleChanges = CheckMapForChanges(l.Examples.Value, r.Examples.Value,
+        &changes, v3.ExamplesLabel, CompareExamples)
+
+    // encoding
+    mc.EncodingChanges = CheckMapForChanges(l.Encoding.Value, r.Encoding.Value,
+        &changes, v3.EncodingLabel, CompareEncoding)
+
+    mc.ExtensionChanges = CompareExtensions(l.Extensions, r.Extensions)
+    mc.Changes = changes
+
+    if mc.TotalChanges() <= 0 {
+        return nil
+    }
+    return mc
 }
