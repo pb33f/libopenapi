3935a0412251bab08c64d8735def082cbe48d87d
Dave Shanley <dave@quobix.com>
2022-12-09 14:45:38 -0500

    Added high level overview report for for what-changed
    Used by `openapi-changes` to provide a high level terminal output.

---
+++ what-changed/reports/types.go
@@ -0,0 +1,14 @@
+// Copyright 2022 Princess B33f Heavy Industries / Dave Shanley
+// SPDX-License-Identifier: MIT
+
+package reports
+
+// HasChanges represents a change model that provides a total change count and a breaking change count.
+type HasChanges interface {
+
+    // TotalChanges represents number of all changes found
+    TotalChanges() int
+
+    // TotalBreakingChanges represents the number of contract breaking changes only.
+    TotalBreakingChanges() int
+}
