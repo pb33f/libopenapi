977218881f032c162b77da26ba8979cc70ae9251
Dave Shanley <dave@quobix.com>
2023-02-20 05:54:28 -0500

    Adressed comments from @n0rig on #83
    Fixed digital ocean test fix, bumped comments up a little

--- datamodel/document_config.go
+++ datamodel/document_config.go
@@ -7,6 +7,9 @@ import "net/url"
 
 // DocumentConfiguration is used to configure the document creation process. It was added in v0.6.0 to allow
 // for more fine-grained control over controls and new features.
+//
+// The default configuration will set AllowFileReferences to false and AllowRemoteReferences to false, which means
+// any non-local (local being the specification, not the file system) references, will be ignored.
 type DocumentConfiguration struct {
     // if the document uses relative file references, this is the base url to use when resolving them.
     BaseURL *url.URL
