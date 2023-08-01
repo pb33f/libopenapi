0d5b47af29bbd18872c82732941313045253079e
Shawn Poulson <spoulson@mailgun.com>
2023-08-01 15:11:35 -0400

    Refactor v2 `Paths` to parse YAML using `TranslatePipeline`.

--- datamodel/high/v2/paths.go
+++ datamodel/high/v2/paths.go
@@ -4,50 +4,44 @@
 package v2
 
 import (
+	"github.com/pb33f/libopenapi/datamodel"
 	"github.com/pb33f/libopenapi/datamodel/high"
-	low "github.com/pb33f/libopenapi/datamodel/low/v2"
+	"github.com/pb33f/libopenapi/datamodel/low"
+	v2low "github.com/pb33f/libopenapi/datamodel/low/v2"
 )
 
 // Paths represents a high-level Swagger / OpenAPI Paths object, backed by a low-level one.
 type Paths struct {
 	PathItems  map[string]*PathItem
 	Extensions map[string]any
-	low        *low.Paths
+	low        *v2low.Paths
 }
 
 // NewPaths creates a new high-level instance of Paths from a low-level one.
-func NewPaths(paths *low.Paths) *Paths {
+func NewPaths(paths *v2low.Paths) *Paths {
 	p := new(Paths)
 	p.low = paths
 	p.Extensions = high.ExtractExtensions(paths.Extensions)
+	pathItems := make(map[string]*PathItem)
 
-	resultChan := make(chan asyncResult[*PathItem])
-	var buildPath = func(path string, pi *low.PathItem, rChan chan<- asyncResult[*PathItem]) {
-		rChan <- asyncResult[*PathItem]{
-			key:    path,
-			result: NewPathItem(pi),
-		}
+	translateFunc := func(key low.KeyReference[string], value low.ValueReference[*v2low.PathItem]) (asyncResult[*PathItem], error) {
+		return asyncResult[*PathItem]{
+			key:    key.Value,
+			result: NewPathItem(value.Value),
+		}, nil
 	}
-	if len(paths.PathItems) > 0 {
-		pathItems := make(map[string]*PathItem)
-		totalPaths := len(paths.PathItems)
-		for k := range paths.PathItems {
-			go buildPath(k.Value, paths.PathItems[k].Value, resultChan)
-		}
-		completedPaths := 0
-		for completedPaths < totalPaths {
-			select {
-			case res := <-resultChan:
-				completedPaths++
-				pathItems[res.key] = res.result
-			}
-		}
-		p.PathItems = pathItems
+	resultFunc := func(result asyncResult[*PathItem]) error {
+		pathItems[result.key] = result.result
+		return nil
 	}
+	_ = datamodel.TranslateMapParallel[low.KeyReference[string], low.ValueReference[*v2low.PathItem], asyncResult[*PathItem]](
+		paths.PathItems, translateFunc, resultFunc,
+	)
+	p.PathItems = pathItems
 	return p
 }
 
 // GoLow returns the low-level Paths instance that backs the high level one.
-func (p *Paths) GoLow() *low.Paths {
+func (p *Paths) GoLow() *v2low.Paths {
 	return p.low
 }
