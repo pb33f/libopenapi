86b1a6252c0a427fa5ebbd78bbaedd119b7fc092
Dave Shanley <dave@quobix.com>
2023-08-09 07:33:19 -0400

    Fixed low-level bug that failed to deal with null/empty hashing.
    Signed-off-by: Dave Shanley <dave@quobix.com>

--- datamodel/low/extraction_functions_test.go
+++ datamodel/low/extraction_functions_test.go
@@ -1669,6 +1669,12 @@ func TestGenerateHashString(t *testing.T) {
 	assert.Equal(t, "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824",
 		GenerateHashString("hello"))
 
+	assert.Equal(t, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+		GenerateHashString(""))
+
+	assert.Equal(t, "",
+		GenerateHashString(nil))
+
 }
 
 func TestGenerateHashString_Pointer(t *testing.T) {
