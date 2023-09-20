8bb5ddde425486fe05c3cb26514f3916f411c7f1
quobix <dave@quobix.com>
2023-09-20 05:59:10 -0400

    Added a little coverage and working on config design
    adding new properties to top level configs should work nicely.
    
    Signed-off-by: quobix <dave@quobix.com>

--- index/index_model.go
+++ index/index_model.go
@@ -105,6 +105,12 @@ type SpecIndexConfig struct {
 	// Use the `BuildIndex()` method on the index to build it out once resolved/ready.
 	AvoidBuildIndex bool
 
+	// If set to true, the index will ignore circular references in polymorphic schemas.
+	IgnorePolymorphicCircularReferences bool // ignore circular references in polymorphic schemas.
+
+	// If set to true, the index will ignore circular references in arrays.
+	IgnoreArrayCircularReferences bool // ignore circular references in arrays.
+
 	// private fields
 	seenRemoteSources *syncmap.Map
 	remoteLock        *sync.Mutex
