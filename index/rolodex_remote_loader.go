010ab374b94a458ae88cfad4e84fdf2ef6ea2dc7
quobix <dave@quobix.com>
2023-11-25 20:36:54 -0500

    added  logger check to remote loader
    Signed-off-by: quobix <dave@quobix.com>

--- index/rolodex_remote_loader.go
+++ index/rolodex_remote_loader.go
@@ -324,7 +324,9 @@ func (i *RemoteFS) Open(remoteURL string) (fs.File, error) {
 	if fileExt == UNSUPPORTED {
 		i.ProcessingFiles.Delete(remoteParsedURL.Path)
 		i.remoteErrors = append(i.remoteErrors, fs.ErrInvalid)
-		i.logger.Warn("[rolodex remote loader] unsupported file in reference will be ignored", "file", remoteURL, "remoteURL", remoteParsedURL.String())
+		if i.logger != nil {
+			i.logger.Warn("[rolodex remote loader] unsupported file in reference will be ignored", "file", remoteURL, "remoteURL", remoteParsedURL.String())
+		}
 		return nil, &fs.PathError{Op: "open", Path: remoteURL, Err: fs.ErrInvalid}
 	}
 
