869346d7cfc5f21386bf002fa552a8b9f21a85a9
quobix <dave@quobix.com>
2023-11-29 09:47:44 -0500

    added error check for local FS
    Signed-off-by: quobix <dave@quobix.com>

--- index/rolodex.go
+++ index/rolodex.go
@@ -439,6 +439,7 @@ func (r *Rolodex) Open(location string) (RolodexFile, error) {
 	if !isUrl {
 		if len(r.localFS) <= 0 {
 			r.logger.Warn("[rolodex] no local file systems configured, cannot open local file", "location", location)
+			return nil, fmt.Errorf("the rolodex has no local file systems configured, cannot open local file '%s'", location)
 		}
 
 		for k, v := range r.localFS {
