dfb02c9568599fa850745d20075bb244121f9559
quobix <dave@quobix.com>
2024-12-23 16:32:44 -0500

    moar coverage!

--- index/index_model_test.go
+++ index/index_model_test.go
@@ -4,7 +4,9 @@
 package index
 
 import (
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
 	"testing"
 )
 
@@ -13,4 +15,45 @@ func TestSpecIndex_GetConfig(t *testing.T) {
 	c := SpecIndexConfig{}
 	idx1.config = &c
 	assert.Equal(t, &c, idx1.GetConfig())
+}
+
+func Test_MarshalJSON(t *testing.T) {
+
+	rm := &ReferenceMapped{
+		OriginalReference: &Reference{
+			FullDefinition: "full definition",
+			Path:           "path",
+			Node: &yaml.Node{
+				Line:   1,
+				Column: 1,
+				Content: []*yaml.Node{
+					{
+						Line:   9,
+						Column: 10,
+					},
+					{
+						Value: "lemon cake",
+					},
+				},
+			},
+		},
+		Reference: &Reference{
+			FullDefinition: "full definition",
+			Path:           "path",
+			Node: &yaml.Node{
+				Line:   2,
+				Column: 2,
+			},
+			KeyNode: &yaml.Node{
+				Line:   3,
+				Column: 3,
+			},
+		},
+		Definition:     "definition",
+		FullDefinition: "full definition",
+		IsPolymorphic:  true,
+	}
+
+	bytes, _ := json.Marshal(rm)
+	assert.Len(t, bytes, 173)
 }
