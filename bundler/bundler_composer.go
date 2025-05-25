bf9d43f04785e76815122c7694ea0a61bf71940e
quobix <dave@quobix.com>
2025-05-25 11:17:09 -0400

    fixed test issues

--- bundler/bundler_composer.go
+++ bundler/bundler_composer.go
@@ -239,7 +239,7 @@ func processReference(model *v3.Document, pr *processRef, cf *handleIndexConfig)
 				l.Warn("[bundler] unable to compose reference, not sure where it goes.", "$ref", pr.ref.FullDefinition)
 			}
 			// no idea what do with this, so we will inline it.
-			//cf.inlineRequired = append(cf.inlineRequired, pr)
+			cf.inlineRequired = append(cf.inlineRequired, pr)
 		}
 	}
 	return nil
