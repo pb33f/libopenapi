e588735325bf16cd3eed4560fdb4ad8bbd7b908b
quobix <dave@quobix.com>
2025-05-26 08:49:44 -0400

    addressed coverage

--- bundler/composer_functions_test.go
+++ bundler/composer_functions_test.go
@@ -2,3 +2,100 @@
 // https://pb33f.io
 
 package bundler
+
+import (
+	"github.com/pb33f/libopenapi"
+	"github.com/pb33f/libopenapi/index"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestProcessRef_UnknownLocation(t *testing.T) {
+
+	// create an empty doc
+	doc, _ := libopenapi.NewDocument([]byte("openapi: 3.1.1"))
+	m, _ := doc.BuildV3Model()
+
+	ref := &processRef{
+		idx: m.Index,
+		ref: &index.Reference{
+			FullDefinition: "#/blarp",
+		},
+		seqRef:   nil,
+		name:     "test",
+		location: []string{"unknown"},
+	}
+
+	config := &handleIndexConfig{
+		compositionConfig: &BundleCompositionConfig{
+			Delimiter: "__",
+		},
+		idx: m.Index,
+	}
+
+	err := processReference(&m.Model, ref, config)
+
+	assert.NoError(t, err)
+	assert.Len(t, config.inlineRequired, 1)
+
+}
+
+func TestProcessRef_UnknownLocation_TwoStep(t *testing.T) {
+
+	// create an empty doc
+	doc, _ := libopenapi.NewDocument([]byte("openapi: 3.1.1"))
+	m, _ := doc.BuildV3Model()
+
+	ref := &processRef{
+		idx: m.Index,
+		ref: &index.Reference{
+			FullDefinition: "#/blarp/blop",
+		},
+		seqRef:   nil,
+		name:     "test",
+		location: []string{"unknown"},
+	}
+
+	config := &handleIndexConfig{
+		compositionConfig: &BundleCompositionConfig{
+			Delimiter: "__",
+		},
+		idx: m.Index,
+	}
+
+	err := processReference(&m.Model, ref, config)
+
+	assert.NoError(t, err)
+	assert.Len(t, config.inlineRequired, 1)
+
+}
+
+func TestProcessRef_UnknownLocation_ThreeStep(t *testing.T) {
+
+	// create an empty doc
+	doc, _ := libopenapi.NewDocument([]byte("openapi: 3.1.1"))
+	m, _ := doc.BuildV3Model()
+
+	ref := &processRef{
+		idx: m.Index,
+		ref: &index.Reference{
+			FullDefinition: "#/blarp/blop/blurp",
+		},
+		seqRef:   nil,
+		name:     "test",
+		location: []string{"unknown"},
+	}
+
+	config := &handleIndexConfig{
+		compositionConfig: &BundleCompositionConfig{
+			Delimiter: "__",
+		},
+		idx: m.Index,
+	}
+
+	err := processReference(&m.Model, ref, config)
+
+	assert.NoError(t, err)
+	assert.Len(t, config.inlineRequired, 1)
+
+}
