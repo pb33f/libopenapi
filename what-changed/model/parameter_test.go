c2ac93200fbe93a4a2703eec879171c17d9dde7f
Sebastiaan Brouwer <sebastiaan.brouwer@nedap.com>
2025-06-30 14:09:42 +0200

    test parameter change optional to required

--- what-changed/model/parameter_test.go
+++ what-changed/model/parameter_test.go
@@ -675,6 +675,62 @@ func TestCompareParameters_V2_Extensions(t *testing.T) {
 	assert.Equal(t, 1, extChanges.ExtensionChanges.TotalChanges())
 }
 
+func TestCompareParameters_V3_OptionalToRequired(t *testing.T) {
+	left := `in: query
+name: burgerId
+required: false`
+
+	right := `in: query
+name: burgerId
+required: true`
+
+	var lNode, rNode yaml.Node
+	_ = yaml.Unmarshal([]byte(left), &lNode)
+	_ = yaml.Unmarshal([]byte(right), &rNode)
+
+	// create low level objects
+	var lDoc v3.Parameter
+	var rDoc v3.Parameter
+	_ = low.BuildModel(lNode.Content[0], &lDoc)
+	_ = low.BuildModel(rNode.Content[0], &rDoc)
+	_ = lDoc.Build(context.Background(), nil, lNode.Content[0], nil)
+	_ = rDoc.Build(context.Background(), nil, rNode.Content[0], nil)
+
+	// compare
+	extChanges := CompareParameters(&lDoc, &rDoc)
+	assert.Equal(t, 1, extChanges.TotalChanges())
+	assert.Len(t, extChanges.GetAllChanges(), 1)
+	assert.Equal(t, 1, extChanges.TotalBreakingChanges())
+}
+
+func TestCompareParameters_V2_OptionalToRequired(t *testing.T) {
+	left := `in: query
+name: burgerId
+required: false`
+
+	right := `in: query
+name: burgerId
+required: true`
+
+	var lNode, rNode yaml.Node
+	_ = yaml.Unmarshal([]byte(left), &lNode)
+	_ = yaml.Unmarshal([]byte(right), &rNode)
+
+	// create low level objects
+	var lDoc v2.Parameter
+	var rDoc v2.Parameter
+	_ = low.BuildModel(lNode.Content[0], &lDoc)
+	_ = low.BuildModel(rNode.Content[0], &rDoc)
+	_ = lDoc.Build(context.Background(), nil, lNode.Content[0], nil)
+	_ = rDoc.Build(context.Background(), nil, rNode.Content[0], nil)
+
+	// compare.
+	extChanges := CompareParameters(&lDoc, &rDoc)
+	assert.Equal(t, 1, extChanges.TotalChanges())
+	assert.Len(t, extChanges.GetAllChanges(), 1)
+	assert.Equal(t, 1, extChanges.TotalBreakingChanges())
+}
+
 func TestCompareParameters_V3_RequiredToOptional(t *testing.T) {
 	left := `in: query
 name: burgerId
