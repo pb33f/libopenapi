84fd63ab62b8bedf3468235179cc810e17fb7b59
quobix <dave@quobix.com>
2025-09-13 13:13:32 -0400

    working on #90 and #262

--- datamodel/document_config_test.go
+++ datamodel/document_config_test.go
@@ -13,3 +13,14 @@ func TestNewClosedDocumentConfiguration(t *testing.T) {
 	cfg := NewDocumentConfiguration()
 	assert.NotNil(t, cfg)
 }
+
+func TestNewDocumentConfiguration_DefaultSiblingRefTransformation(t *testing.T) {
+	cfg := NewDocumentConfiguration()
+	assert.True(t, cfg.TransformSiblingRefs, "TransformSiblingRefs should be enabled by default for OpenAPI 3.1 compliance")
+}
+
+func TestDocumentConfiguration_SiblingRefTransformationDisabled(t *testing.T) {
+	cfg := NewDocumentConfiguration()
+	cfg.TransformSiblingRefs = false
+	assert.False(t, cfg.TransformSiblingRefs, "TransformSiblingRefs should be configurable")
+}
