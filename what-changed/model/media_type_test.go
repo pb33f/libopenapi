ce8128a1573d7fd6f7add94b8429fc514311a2bc
quobix <dave@quobix.com>
2025-09-21 16:26:28 -0400

    cleaning and dusting and fixing flaky tests

--- what-changed/model/media_type_test.go
+++ what-changed/model/media_type_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/pb33f/libopenapi/datamodel/low"
 	v3 "github.com/pb33f/libopenapi/datamodel/low/v3"
+	"github.com/pb33f/libopenapi/index"
 	"github.com/stretchr/testify/assert"
 	"go.yaml.in/yaml/v4"
 )
@@ -329,4 +330,386 @@ x-tea: cup`
 	assert.Equal(t, 6, extChanges.TotalChanges())
 	assert.Len(t, extChanges.GetAllChanges(), 6)
 	assert.Equal(t, 3, extChanges.TotalBreakingChanges())
+}
+
+func TestCompareMediaTypes_ItemSchemaAdded(t *testing.T) {
+	// Clear hash cache for deterministic testing
+	low.ClearHashCache()
+
+	left := `schema:
+  type: array`
+
+	right := `schema:
+  type: array
+itemSchema:
+  type: object
+  properties:
+    id:
+      type: string`
+
+	var lNode, rNode yaml.Node
+	_ = yaml.Unmarshal([]byte(left), &lNode)
+	_ = yaml.Unmarshal([]byte(right), &rNode)
+
+	lIdx := index.NewSpecIndex(&lNode)
+	rIdx := index.NewSpecIndex(&rNode)
+
+	var lMt, rMt v3.MediaType
+	_ = low.BuildModel(&lNode, &lMt)
+	_ = low.BuildModel(&rNode, &rMt)
+	_ = lMt.Build(context.Background(), nil, lNode.Content[0], lIdx)
+	_ = rMt.Build(context.Background(), nil, rNode.Content[0], rIdx)
+
+	changes := CompareMediaTypes(&lMt, &rMt)
+
+	assert.NotNil(t, changes)
+	assert.Nil(t, changes.ItemSchemaChanges) // No changes, just addition
+	assert.Equal(t, 1, changes.TotalChanges())
+	assert.Equal(t, 1, changes.TotalBreakingChanges()) // Adding itemSchema is breaking
+
+	// Check that the change is ObjectAdded for itemSchema
+	allChanges := changes.GetAllChanges()
+	assert.Len(t, allChanges, 1)
+	assert.Equal(t, ObjectAdded, allChanges[0].ChangeType)
+	assert.Equal(t, v3.ItemSchemaLabel, allChanges[0].Property)
+}
+
+func TestCompareMediaTypes_ItemSchemaRemoved(t *testing.T) {
+	// Clear hash cache for deterministic testing
+	low.ClearHashCache()
+
+	left := `schema:
+  type: array
+itemSchema:
+  type: object
+  properties:
+    id:
+      type: string`
+
+	right := `schema:
+  type: array`
+
+	var lNode, rNode yaml.Node
+	_ = yaml.Unmarshal([]byte(left), &lNode)
+	_ = yaml.Unmarshal([]byte(right), &rNode)
+
+	lIdx := index.NewSpecIndex(&lNode)
+	rIdx := index.NewSpecIndex(&rNode)
+
+	var lMt, rMt v3.MediaType
+	_ = low.BuildModel(&lNode, &lMt)
+	_ = low.BuildModel(&rNode, &rMt)
+	_ = lMt.Build(context.Background(), nil, lNode.Content[0], lIdx)
+	_ = rMt.Build(context.Background(), nil, rNode.Content[0], rIdx)
+
+	changes := CompareMediaTypes(&lMt, &rMt)
+
+	assert.NotNil(t, changes)
+	assert.Nil(t, changes.ItemSchemaChanges) // No changes, just removal
+	assert.Equal(t, 1, changes.TotalChanges())
+	assert.Equal(t, 1, changes.TotalBreakingChanges()) // Removing itemSchema is breaking
+
+	// Check that the change is ObjectRemoved for itemSchema
+	allChanges := changes.GetAllChanges()
+	assert.Len(t, allChanges, 1)
+	assert.Equal(t, ObjectRemoved, allChanges[0].ChangeType)
+	assert.Equal(t, v3.ItemSchemaLabel, allChanges[0].Property)
+}
+
+func TestCompareMediaTypes_ItemSchemaModified(t *testing.T) {
+	// Clear hash cache for deterministic testing
+	low.ClearHashCache()
+
+	left := `schema:
+  type: array
+itemSchema:
+  type: object
+  properties:
+    id:
+      type: string
+    name:
+      type: string`
+
+	right := `schema:
+  type: array
+itemSchema:
+  type: object
+  properties:
+    id:
+      type: integer
+    name:
+      type: string
+    age:
+      type: integer`
+
+	var lNode, rNode yaml.Node
+	_ = yaml.Unmarshal([]byte(left), &lNode)
+	_ = yaml.Unmarshal([]byte(right), &rNode)
+
+	lIdx := index.NewSpecIndex(&lNode)
+	rIdx := index.NewSpecIndex(&rNode)
+
+	var lMt, rMt v3.MediaType
+	_ = low.BuildModel(&lNode, &lMt)
+	_ = low.BuildModel(&rNode, &rMt)
+	_ = lMt.Build(context.Background(), nil, lNode.Content[0], lIdx)
+	_ = rMt.Build(context.Background(), nil, rNode.Content[0], rIdx)
+
+	changes := CompareMediaTypes(&lMt, &rMt)
+
+	assert.NotNil(t, changes)
+	assert.NotNil(t, changes.ItemSchemaChanges)
+	assert.Greater(t, changes.TotalChanges(), 0)
+	assert.Greater(t, changes.TotalBreakingChanges(), 0) // Schema changes are breaking
+}
+
+func TestCompareMediaTypes_ItemEncodingAdded(t *testing.T) {
+	// Clear hash cache for deterministic testing
+	low.ClearHashCache()
+
+	left := `schema:
+  type: array`
+
+	right := `schema:
+  type: array
+itemEncoding:
+  file:
+    contentType: image/jpeg
+    allowReserved: true`
+
+	var lNode, rNode yaml.Node
+	_ = yaml.Unmarshal([]byte(left), &lNode)
+	_ = yaml.Unmarshal([]byte(right), &rNode)
+
+	lIdx := index.NewSpecIndex(&lNode)
+	rIdx := index.NewSpecIndex(&rNode)
+
+	var lMt, rMt v3.MediaType
+	_ = low.BuildModel(&lNode, &lMt)
+	_ = low.BuildModel(&rNode, &rMt)
+	_ = lMt.Build(context.Background(), nil, lNode.Content[0], lIdx)
+	_ = rMt.Build(context.Background(), nil, rNode.Content[0], rIdx)
+
+	changes := CompareMediaTypes(&lMt, &rMt)
+
+	assert.NotNil(t, changes)
+	assert.Greater(t, changes.TotalChanges(), 0)
+
+	// When a whole encoding block is added, it's tracked in the changes array, not ItemEncodingChanges
+	allChanges := changes.GetAllChanges()
+	assert.Len(t, allChanges, 1)
+	assert.Equal(t, ObjectAdded, allChanges[0].ChangeType)
+	assert.Equal(t, v3.ItemEncodingLabel, allChanges[0].Property)
+}
+
+func TestCompareMediaTypes_ItemEncodingModified(t *testing.T) {
+	// Clear hash cache for deterministic testing
+	low.ClearHashCache()
+
+	left := `schema:
+  type: array
+itemEncoding:
+  file:
+    contentType: image/jpeg
+    allowReserved: true`
+
+	right := `schema:
+  type: array
+itemEncoding:
+  file:
+    contentType: image/png
+    allowReserved: false`
+
+	var lNode, rNode yaml.Node
+	_ = yaml.Unmarshal([]byte(left), &lNode)
+	_ = yaml.Unmarshal([]byte(right), &rNode)
+
+	lIdx := index.NewSpecIndex(&lNode)
+	rIdx := index.NewSpecIndex(&rNode)
+
+	var lMt, rMt v3.MediaType
+	_ = low.BuildModel(&lNode, &lMt)
+	_ = low.BuildModel(&rNode, &rMt)
+	_ = lMt.Build(context.Background(), nil, lNode.Content[0], lIdx)
+	_ = rMt.Build(context.Background(), nil, rNode.Content[0], rIdx)
+
+	changes := CompareMediaTypes(&lMt, &rMt)
+
+	assert.NotNil(t, changes)
+	assert.NotNil(t, changes.ItemEncodingChanges)
+	assert.Equal(t, 1, len(changes.ItemEncodingChanges))
+	assert.Greater(t, changes.TotalChanges(), 0)
+}
+
+func TestCompareMediaTypes_BothEncodingTypes(t *testing.T) {
+	// Clear hash cache for deterministic testing
+	low.ClearHashCache()
+
+	left := `schema:
+  type: object
+encoding:
+  field1:
+    contentType: text/plain
+itemEncoding:
+  stream1:
+    contentType: application/json`
+
+	right := `schema:
+  type: object
+encoding:
+  field1:
+    contentType: text/html
+itemEncoding:
+  stream1:
+    contentType: application/xml`
+
+	var lNode, rNode yaml.Node
+	_ = yaml.Unmarshal([]byte(left), &lNode)
+	_ = yaml.Unmarshal([]byte(right), &rNode)
+
+	lIdx := index.NewSpecIndex(&lNode)
+	rIdx := index.NewSpecIndex(&rNode)
+
+	var lMt, rMt v3.MediaType
+	_ = low.BuildModel(&lNode, &lMt)
+	_ = low.BuildModel(&rNode, &rMt)
+	_ = lMt.Build(context.Background(), nil, lNode.Content[0], lIdx)
+	_ = rMt.Build(context.Background(), nil, rNode.Content[0], rIdx)
+
+	changes := CompareMediaTypes(&lMt, &rMt)
+
+	assert.NotNil(t, changes)
+	assert.NotNil(t, changes.EncodingChanges)
+	assert.NotNil(t, changes.ItemEncodingChanges)
+	assert.Equal(t, 1, len(changes.EncodingChanges))
+	assert.Equal(t, 1, len(changes.ItemEncodingChanges))
+	assert.Greater(t, changes.TotalChanges(), 1) // At least 2 changes
+}
+
+func TestCompareMediaTypes_NoChangesWithItemFields(t *testing.T) {
+	// Clear hash cache for deterministic testing
+	low.ClearHashCache()
+
+	yml := `schema:
+  type: array
+itemSchema:
+  type: object
+  properties:
+    id:
+      type: string
+itemEncoding:
+  id:
+    contentType: text/plain`
+
+	var lNode, rNode yaml.Node
+	_ = yaml.Unmarshal([]byte(yml), &lNode)
+	_ = yaml.Unmarshal([]byte(yml), &rNode)
+
+	lIdx := index.NewSpecIndex(&lNode)
+	rIdx := index.NewSpecIndex(&rNode)
+
+	var lMt, rMt v3.MediaType
+	_ = low.BuildModel(&lNode, &lMt)
+	_ = low.BuildModel(&rNode, &rMt)
+	_ = lMt.Build(context.Background(), nil, lNode.Content[0], lIdx)
+	_ = rMt.Build(context.Background(), nil, rNode.Content[0], rIdx)
+
+	changes := CompareMediaTypes(&lMt, &rMt)
+
+	assert.Nil(t, changes) // No changes
+}
+
+func TestMediaTypeChanges_GetAllChanges_WithItemFields(t *testing.T) {
+	// Clear hash cache for deterministic testing
+	low.ClearHashCache()
+
+	left := `schema:
+  type: array`
+
+	right := `schema:
+  type: array
+itemSchema:
+  type: object
+itemEncoding:
+  field:
+    contentType: application/json`
+
+	var lNode, rNode yaml.Node
+	_ = yaml.Unmarshal([]byte(left), &lNode)
+	_ = yaml.Unmarshal([]byte(right), &rNode)
+
+	lIdx := index.NewSpecIndex(&lNode)
+	rIdx := index.NewSpecIndex(&rNode)
+
+	var lMt, rMt v3.MediaType
+	_ = low.BuildModel(&lNode, &lMt)
+	_ = low.BuildModel(&rNode, &rMt)
+	_ = lMt.Build(context.Background(), nil, lNode.Content[0], lIdx)
+	_ = rMt.Build(context.Background(), nil, rNode.Content[0], rIdx)
+
+	changes := CompareMediaTypes(&lMt, &rMt)
+
+	assert.NotNil(t, changes)
+	allChanges := changes.GetAllChanges()
+	assert.GreaterOrEqual(t, len(allChanges), 2) // At least itemSchema and itemEncoding changes
+
+	// Verify we can find both types of changes
+	var foundItemSchema, foundItemEncoding bool
+	for _, change := range allChanges {
+		if change.Property == v3.ItemSchemaLabel {
+			foundItemSchema = true
+		}
+		if change.Property == v3.ItemEncodingLabel {
+			foundItemEncoding = true
+		}
+	}
+	assert.True(t, foundItemSchema, "Should find itemSchema change")
+	assert.True(t, foundItemEncoding, "Should find itemEncoding change")
+}
+
+func TestMediaTypeChanges_TotalBreakingChanges_WithItemSchema(t *testing.T) {
+	// Clear hash cache for deterministic testing
+	low.ClearHashCache()
+
+	left := `schema:
+  type: array
+itemSchema:
+  type: object
+  required:
+    - id
+  properties:
+    id:
+      type: string`
+
+	right := `schema:
+  type: array
+itemSchema:
+  type: object
+  required:
+    - id
+    - name
+  properties:
+    id:
+      type: string
+    name:
+      type: string`
+
+	var lNode, rNode yaml.Node
+	_ = yaml.Unmarshal([]byte(left), &lNode)
+	_ = yaml.Unmarshal([]byte(right), &rNode)
+
+	lIdx := index.NewSpecIndex(&lNode)
+	rIdx := index.NewSpecIndex(&rNode)
+
+	var lMt, rMt v3.MediaType
+	_ = low.BuildModel(&lNode, &lMt)
+	_ = low.BuildModel(&rNode, &rMt)
+	_ = lMt.Build(context.Background(), nil, lNode.Content[0], lIdx)
+	_ = rMt.Build(context.Background(), nil, rNode.Content[0], rIdx)
+
+	changes := CompareMediaTypes(&lMt, &rMt)
+
+	assert.NotNil(t, changes)
+	assert.NotNil(t, changes.ItemSchemaChanges)
+	assert.Greater(t, changes.TotalBreakingChanges(), 0) // Adding required field is breaking
 }
