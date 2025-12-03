8bc7c8cc304ab0b5fb8b3619bfff325d901f7998
quobix <dave@quobix.com>
2025-12-02 19:40:39 -0500

    addressing coverage on new last ditch lookup

--- index/search_index_test.go
+++ index/search_index_test.go
@@ -40,3 +40,55 @@ func TestSpecIndex_SearchIndexForReferenceWithContext(t *testing.T) {
 	assert.Nil(t, idx.GetRootNode())
 
 }
+
+// TestSearchIndexForReference_LastDitchRolodexFallback tests the last-ditch effort
+// code path where a reference is found by iterating through rolodex indexes
+// after all other lookup methods fail.
+func TestSearchIndexForReference_LastDitchRolodexFallback(t *testing.T) {
+	// Primary index with NO components - searches will fail here
+	primarySpec := `openapi: 3.0.1
+info:
+  title: Primary
+  version: "1.0"`
+
+	var primaryRoot yaml.Node
+	_ = yaml.Unmarshal([]byte(primarySpec), &primaryRoot)
+
+	c := CreateOpenAPIIndexConfig()
+	primaryIdx := NewSpecIndexWithConfig(&primaryRoot, c)
+
+	// Secondary index WITH the component we want to find
+	secondarySpec := `openapi: 3.0.1
+info:
+  title: Secondary
+  version: "1.0"
+components:
+  schemas:
+    Pet:
+      type: object
+      properties:
+        name:
+          type: string`
+
+	var secondaryRoot yaml.Node
+	_ = yaml.Unmarshal([]byte(secondarySpec), &secondaryRoot)
+
+	secondaryIdx := NewSpecIndexWithConfig(&secondaryRoot, c)
+
+	// Create rolodex and add secondary index
+	rolo := NewRolodex(c)
+	rolo.AddIndex(secondaryIdx)
+
+	// Set rolodex on primary index
+	primaryIdx.SetRolodex(rolo)
+
+	// Search for reference that:
+	// 1. Doesn't exist in primary index's allMappedRefs
+	// 2. Has roloLookup = "" (simple ref format)
+	// 3. Should be found via last-ditch rolodex iteration
+	ref, idx := primaryIdx.SearchIndexForReference("#/components/schemas/Pet")
+
+	assert.NotNil(t, ref, "Reference should be found via rolodex fallback")
+	assert.NotNil(t, idx, "Index should be returned")
+	assert.Equal(t, "Pet", ref.Name)
+}
