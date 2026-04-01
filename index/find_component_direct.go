3257cbc36aeadfa3371264c4120293c536b7be5d
quobix <dave@quobix.com>
2026-04-01 10:55:32 -0400

    more header changes.

--- index/find_component_direct.go
+++ index/find_component_direct.go
@@ -1,4 +1,4 @@
-// Copyright 2023 Princess B33f Heavy Industries / Dave Shanley
+// Copyright 2023-2026 Princess B33f Heavy Industries / Dave Shanley
 // SPDX-License-Identifier: MIT
 
 package index
